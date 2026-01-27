e2a4533b7b26349372f982220909da40e060c7b7
Apolo Medina Cshunderlick <apololick2@gmail.com>
2026-01-27 10:56:56 -0300

    tentando

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,12 @@ import { HashRouter } from 'react-router-dom'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 import { RoutesPage } from './routes/routes'
 import { StyleReset } from './styles/globalStyleReset'
-import { FooterPage } from './components/footer'
-import { Headerpage } from './components/header'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
-      <Headerpage />
       <RoutesPage />
       <StyleReset />
-      <FooterPage />
     </HashRouter>
   </StrictMode>,
 )
